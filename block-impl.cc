4f8c1becb40fd76000e1206c02cfe2dba0f698a8
Anirudh Chhabra <185633507+chhabra-anirudh@users.noreply.github.com>
2025-11-23 22:26:56 -0500

    Implemented the code for Abstract Block

---
+++ block-impl.cc
@@ -0,0 +1,94 @@
+module Block;
+
+import <vector>;
+import <algorithm>;
+import Position;
+import Cell;
+
+using namespace std;
+
+Block::Block(char type, int level, bool heavy) 
+    : blockType{type}, level{level}, bottomLeft{0, 0}, orientation{0}, 
+    isHeavy{heavy}, isPlaced{false}, cellsFilled{4} {}
+
+Block::~Block(){}
+
+void Block::setPosition(const Position &pos) {
+    bottomLeft = pos;
+}
+
+Position Block::getPosition() const {
+    return bottomLeft;
+}
+
+void Block::rotateClockwise() {
+    orientation = (orientation + 1) % 4;
+}
+
+void Block::rotateCounterClockwise() {
+    orientation = (orientation + 3) % 4;
+}
+
+char Block::getType() const {
+    return blockType;
+}
+
+int Block::getLevel() const {
+    return level;
+}
+
+bool Block::heavy() const {
+    return isHeavy;
+}
+
+void Block::setHeavy(bool heavy) {
+    isHeavy = heavy;
+}
+
+bool Block::placed() const {
+    return isPlaced;
+}
+
+void Block::setPlaced(bool placed) {
+    isPlaced = placed;
+}
+
+void Block::addCell(Cell *cell) {
+    cells.emplace_back(cell);
+}
+
+void Block::removeCell(Cell *cell) {
+    for (auto it = cells.begin(); it != cells.end();) {
+        if (*it == cell) it = cells.erase(it);
+        else ++it;
+    }
+}
+
+const vector<Cell*> &Block::getCells() const {
+    return cells;
+}
+
+bool Block::isFilled() {
+    return cellsFilled > 0;
+}
+
+void Block::decrementCell() {
+    if (cellsFilled > 0) {
+        --cellsFilled;
+    }
+}
+
+int Block::getOrientation() const {
+    return orientation;
+}
+
+vector<Position> Block::getCurrentPositions() const {
+    vector<Position> shape = getShapeCoordinates();
+    vector<Position> absolute;
+
+    for (int i = 0; i < shape.size(); i++) {
+        absolute.emplace_back(Position {(bottomLeft.row + shape[i].row), (bottomLeft.col + shape[i].col)});
+    }
+
+    return absolute;
+}
---
+++ block.cc
@@ -0,0 +1,56 @@
+export module Block;
+
+import <vector>;
+import Position;
+
+class Cell;
+
+using namespace std;
+
+export class Block {
+    char blockType; // 'I', 'J', 'L', 'O', 'S', 'Z', 'T', '*'
+    int level; // Level at which block was generated
+    vector<Cell*> cells; // Cells occupied by this block
+    Position bottomLeft; // Bottom-left corner of bounding box
+    int orientation; // 0, 1, 2, 3 for rotation states
+    bool isHeavy; // Heavy blocks drop automatically
+    bool isPlaced; // Has the block been placed on board?
+    int cellsFilled; // Number of cells still part of this block
+
+    protected:
+        // Subclasses (Blocks) define their shape using this pure virtual function
+        virtual vector<Position> getShapeCoordinates() const = 0;
+
+    public:
+        Block(char type, int level, bool heavy = false);
+        virtual ~Block();
+
+        // Movement and rotation
+        void setPosition(const Position &pos);
+        Position getPosition() const;
+
+        void rotateClockwise();
+        void rotateCounterClockwise();
+
+        // Get current cell positions relative to board
+        vector<Position> getCurrentPositions() const;
+
+        // Block state
+        char getType() const;
+        int getLevel() const;
+        bool heavy() const;
+        void setHeavy(bool heavy);
+        bool placed() const;
+        void setPlaced(bool placed);
+
+        // Cell management
+        void addCell(Cell *cell);
+        void removeCell(Cell *cell);
+        const vector<Cell*> &getCells() const;
+
+        // Check if block is fully cleared
+        bool isFilled();
+        void decrementCell();
+
+        int getOrientation() const;
+};
